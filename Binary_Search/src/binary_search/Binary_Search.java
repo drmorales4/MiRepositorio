2d3fb39393d9d24d0764e0e5f218d009afc992d4
drmorales4 <drmorales4@gmail.com>
2020-05-12 20:36:38 -0500

    .

--- Binary_Search/src/binary_search/Binary_Search.java
+++ Binary_Search/src/binary_search/Binary_Search.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package binary_search;
 
 import java.util.Random;
 
-/**
- *
- * @author drmorales4
- */
 public class Binary_Search {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Random aleatorio = new Random(System.currentTimeMillis());
 
         int[] values = new int[1000];
@@ -37,19 +24,4 @@ public class Binary_Search {
             break;
         }
     }
-
-    /*
-    public static void metodo(int [] values){
-        
-        for (int i = 0; i < 10000000; i++) {
-            int index = -1;
-            for (int j = 0; j < values.length; j++) {
-                if (values[j] == 80) {
-                    index = j;
-                }
-            }
-            break;
-        }
-    }
-     */
 }
